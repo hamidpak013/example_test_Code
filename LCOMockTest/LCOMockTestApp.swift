9b9ac96a76b2bc1345af89c815d6c19483406fcf
Hamid <hamid.pak013@gmail.com>
2021-10-01 23:52:49 +0500

    Initial Commit

---
+++ LCOMockTest/LCOMockTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCOMockTestApp.swift
+//  LCOMockTest
+//
+//  Created by Hamid Ali on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LCOMockTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
